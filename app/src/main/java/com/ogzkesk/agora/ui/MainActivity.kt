78447156ecc3b680c6b73c3934adb8faff2ff348
ogzkesk <ogz.kskn57@gmail.com>
2025-03-02 21:32:03 +0300

    update

--- app/src/main/java/com/ogzkesk/agora/ui/MainActivity.kt
+++ app/src/main/java/com/ogzkesk/agora/ui/MainActivity.kt
@@ -1,19 +1,12 @@
 package com.ogzkesk.agora.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.getValue
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import com.ogzkesk.agora.ui.main.MainScreen
-import com.ogzkesk.agora.ui.main.MainViewModel
 import com.ogzkesk.agora.navigation.MainNavHost
-import com.ogzkesk.agora.navigation.MainScreenRoute
+import com.ogzkesk.agora.service.LocalRecordingService
 import com.ogzkesk.agora.ui.theme.AgoraTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -27,5 +20,11 @@ class MainActivity : ComponentActivity() {
                 MainNavHost()
             }
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        val serviceIntent = Intent(this, LocalRecordingService::class.java)
+        stopService(serviceIntent)
     }
 }
